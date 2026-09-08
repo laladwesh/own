42558c90b013fd5b8cb72c953e9abe6f02980f22
laladwesh <guptaavinash302@gmail.com>
2026-09-09 04:00:20 +0530

    Reapply "done"
    This reverts commit 963fd696ca1ccfbee5d762aa106edc3adb0c1b0f.

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -8,7 +8,6 @@ import { achievements } from "../constants";
 import styles from "../style";
 import { SectionHeading, TiltCard, Reveal } from "./ui";
 
-// Pick a tasteful trophy/medal/award accent per card (cycles, deterministic by index)
 const accentIcons = [FaTrophy, FaMedal, FaAward];
 
 const Achievements = () => {
