56ec5c176be112a1528ea45158017fa2d672cab7
laladwesh <guptaavinash302@gmail.com>
2026-09-15 09:22:29 +0530

    fix: correct overflow property in App component for proper layout

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const App = () => {
 
   return (
     // A div to wrap the entire application
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary w-full overflow-x-hidden">
       <AnimatePresence>
         {isLoading ? (
           <Loading key="loading" />
